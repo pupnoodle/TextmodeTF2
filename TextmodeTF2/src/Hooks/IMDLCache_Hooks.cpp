0fdb23cd86c0f43b4240c8ecbe1494c26249c26f
vel <dobryfraudzichaxd@proton.me>
2026-04-17 21:48:41 +0200

    fixes

--- TextmodeTF2/src/Hooks/IMDLCache_Hooks.cpp
+++ TextmodeTF2/src/Hooks/IMDLCache_Hooks.cpp
@@ -1,20 +1,37 @@
 #include "../SDK/SDK.h"
 
+namespace
+{
+	bool ShouldBlockModel(IMDLCache* pCache, MDLHandle_t handle)
+	{
+		if (!pCache)
+			return false;
+
+		const char* pModelName = pCache->GetModelName(handle);
+		return pModelName && SDK::BlacklistFile(pModelName);
+	}
+}
+
 MAKE_HOOK(IMDLCache_GetHardwareData, U::Memory.FindSignature("datacache.dll", "48 89 5C 24 08 48 89 74 24 10 57 48 83 EC 20 48 8B 05 ? ? ? ?"), void*,
 		  void* rcx, MDLHandle_t handle)
 {
+	if (ShouldBlockModel(reinterpret_cast<IMDLCache*>(rcx), handle))
+		return nullptr;
+
 	return CALL_ORIGINAL(rcx, handle);
 }
 
 MAKE_HOOK(IMDLCache_GetVertexData, U::Memory.FindSignature("datacache.dll", "48 83 EC ? 48 8B 05 ? ? ? ? 83 78 ? ? 75 ? 48 8B 05 ? ? ? ?"), void*,
 		  void* rcx, MDLHandle_t handle)
 {
+	if (ShouldBlockModel(reinterpret_cast<IMDLCache*>(rcx), handle))
+		return nullptr;
+
 	return CALL_ORIGINAL(rcx, handle);
 }
 
 MAKE_HOOK(IMDLCache_TouchAllData, U::Memory.GetVFunc(reinterpret_cast<void*>(G::IMDLCache), 45), bool,
 		  void* rcx, MDLHandle_t handle)
 {
-	// i think this could make catbots miss
-	return CALL_ORIGINAL(rcx, handle);
+	return true;
 }
--- TextmodeTF2/src/Hooks/IMDLCache_ProcessDataIntoCache.cpp
+++ TextmodeTF2/src/Hooks/IMDLCache_ProcessDataIntoCache.cpp
@@ -1,7 +1,32 @@
 #include "../SDK/SDK.h"
 
+namespace
+{
+	bool ShouldSkipModelData(IMDLCache* pCache, MDLHandle_t handle, MDLCacheDataType_t type)
+	{
+		if (!pCache)
+			return false;
+
+		const char* pModelName = pCache->GetModelName(handle);
+		if (pModelName && SDK::BlacklistFile(pModelName))
+			return true;
+
+		switch (type)
+		{
+		case MDLCACHE_STUDIOHWDATA:
+		case MDLCACHE_VERTEXES:
+			return true;
+		default:
+			return false;
+		}
+	}
+}
+
 MAKE_HOOK(IMDLCache_ProcessDataIntoCache, U::Memory.GetVFunc(reinterpret_cast<void*>(G::IMDLCache), 33), bool,
 		  void* rcx, MDLHandle_t handle, MDLCacheDataType_t type, void* pData, int nDataSize, bool bAsync)
 {
+	if (ShouldSkipModelData(reinterpret_cast<IMDLCache*>(rcx), handle, type))
+		return true;
+
 	return CALL_ORIGINAL(rcx, handle, type, pData, nDataSize, bAsync);
 }
